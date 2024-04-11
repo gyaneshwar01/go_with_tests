79722da2a756ca6b9e73929784a29629ffd0f806
gyaneshwar01 <gyaneshwar585@gmail.com>
2024-04-11 10:34:34 +0545

    dictionary CRUD

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,10 +1,18 @@
 package main
 
-import "errors"
+const (
+	ErrNotFound         = DictionaryErr("couldn't find the word you were looking for")
+	ErrWordExists       = DictionaryErr("word already exists")
+	ErrWordDoesNotExist = DictionaryErr("word does not exist")
+)
+
+type DictionaryErr string
 
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("couldn't find the word you were looking for")
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -16,6 +24,36 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word, newDefinition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = newDefinition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
 }
