7f86caa4ea9d31ad57bbd52314f2ba9e486e441b
gyaneshwar01 <gyaneshwar585@gmail.com>
2024-04-13 20:29:23 +0545

    Countdown

---
+++ mocking/countdown.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(w io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i >= 1; i-- {
+		fmt.Fprintln(w, i)
+		sleeper.Sleep()
+	}
+	fmt.Fprint(w, finalWord)
+}
+
+func main() {
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
+}
