69a5a0e9bd7a347a8470dd8a450f9274f96e9a83
gyaneshwar01 <gyaneshwar585@gmail.com>
2024-04-16 17:31:35 +0545

    wg and mutex

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -1,16 +1,44 @@
 package main
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
-		counter := Counter{}
+		counter := NewCounter()
 		counter.Inc()
 		counter.Inc()
 		counter.Inc()
 
-		if counter.Value() != 3 {
-			t.Errorf("got %d, want %d", counter.Value(), 3)
-		}
+		assertCounter(t, counter, 3)
 	})
+
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := NewCounter()
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				defer wg.Done()
+				counter.Inc()
+			}()
+		}
+
+		wg.Wait()
+
+		assertCounter(t, counter, wantedCount)
+	})
+}
+
+func assertCounter(t testing.TB, counter *Counter, want int) {
+	t.Helper()
+
+	if counter.Value() != want {
+		t.Errorf("got %d, want %d", counter.Value(), want)
+	}
 }
