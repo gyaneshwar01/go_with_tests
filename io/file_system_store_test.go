935c8cb2f48f752d8e4aaf76e318d903c443cfbe
gyaneshwar01 <gyaneshwar585@gmail.com>
2024-04-25 12:29:24 +0545

    storing data in json file

--- io/file_system_store_test.go
+++ io/file_system_store_test.go
@@ -1,23 +1,25 @@
 package main
 
 import (
-	"strings"
+	"io"
+	"os"
 	"testing"
 )
 
 func TestFileSystemStore(t *testing.T) {
 	t.Run("league from a reader", func(t *testing.T) {
-		database := strings.NewReader(`[
-			{"Name": "Cleo", "Wins": 10},
-			{"Name": "Chris", "Wins": 33}
+		database, cleanDatabase := createTempFile(t, `[
+			{"Name": "Aman", "Wins": 10},
+			{"Name": "Sujal", "Wins": 32}
 		]`)
 
+		defer cleanDatabase()
 		store := FileSystemPlayerStore{database}
-
 		got := store.GetLeague()
+
 		want := []Player{
-			{"Cleo", 10},
-			{"Chris", 33},
+			{"Aman", 10},
+			{"Sujal", 32},
 		}
 
 		assertLeague(t, got, want)
@@ -27,20 +29,77 @@ func TestFileSystemStore(t *testing.T) {
 	})
 
 	t.Run("get player score", func(t *testing.T) {
-		database := strings.NewReader(`[
-			{"Name": "Cleo", "Wins": 10},
-			{"Name": "Chris", "Wins": 23}
+		database, cleanDatabase := createTempFile(t, `[
+			{"Name": "Aman", "Wins": 10},
+			{"Name": "Sujal", "Wins": 32}
 		]`)
+
+		defer cleanDatabase()
 
 		store := FileSystemPlayerStore{database}
 
-		got := store.GetPlayerScore("Chris")
-		want := 23
+		got := store.GetPlayerScore("Aman")
+		want := 10
+
+		assertScoreEquals(t, got, want)
+	})
+
+	t.Run("store wins for existing players", func(t *testing.T) {
+		database, cleanDatabase := createTempFile(t, `[
+			{"Name": "Aman", "Wins": 10},
+			{"Name": "Sujal", "Wins": 32}
+		]`)
+
+		defer cleanDatabase()
+
+		store := FileSystemPlayerStore{database}
+
+		store.RecordWin("Sujal")
+
+		got := store.GetPlayerScore("Sujal")
+		want := 33
+		assertScoreEquals(t, got, want)
+	})
+
+	t.Run("store wins for new players", func(t *testing.T) {
+		database, cleanDatabase := createTempFile(t, `[
+			{"Name": "Aman", "Wins": 10},
+			{"Name": "Sujal", "Wins": 32}
+		]`)
+
+		defer cleanDatabase()
+
+		store := FileSystemPlayerStore{database}
+		store.RecordWin("Nitesh")
+
+		got := store.GetPlayerScore("Nitesh")
+		want := 1
+
 		assertScoreEquals(t, got, want)
 	})
 }
 
+func createTempFile(t testing.TB, initialdata string) (io.ReadWriteSeeker, func()) {
+	t.Helper()
+
+	tmpFile, err := os.CreateTemp("", "db")
+	if err != nil {
+		t.Fatalf("could not create a temp file, %v", err)
+	}
+
+	tmpFile.Write([]byte(initialdata))
+
+	removeFile := func() {
+		tmpFile.Close()
+		os.Remove(tmpFile.Name())
+	}
+
+	return tmpFile, removeFile
+}
+
 func assertScoreEquals(t testing.TB, got, want int) {
+	t.Helper()
+
 	if got != want {
 		t.Errorf("got %d, want %d", got, want)
 	}
