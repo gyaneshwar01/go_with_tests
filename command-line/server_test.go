bf1e50234480f1a1de8c072652546fb94ee6174c
gyaneshwar01 <gyaneshwar585@gmail.com>
2024-04-27 12:30:26 +0545

    cli and package structure

--- command-line/server_test.go
+++ command-line/server_test.go
@@ -1,4 +1,4 @@
-package main
+package poker
 
 import (
 	"fmt"
@@ -8,24 +8,6 @@ import (
 	"slices"
 	"testing"
 )
-
-type StubPlayerStore struct {
-	scores   map[string]int
-	winCalls []string
-	league   []Player
-}
-
-func (s *StubPlayerStore) GetPlayerScore(name string) int {
-	return s.scores[name]
-}
-
-func (s *StubPlayerStore) RecordWin(name string) {
-	s.winCalls = append(s.winCalls, name)
-}
-
-func (s *StubPlayerStore) GetLeague() League {
-	return s.league
-}
 
 func TestGetPlayers(t *testing.T) {
 	store := StubPlayerStore{
@@ -74,13 +56,7 @@ func TestScoreWins(t *testing.T) {
 
 		assertStatus(t, response.Code, http.StatusAccepted)
 
-		if len(store.winCalls) != 1 {
-			t.Errorf("got %d calls to RecordWin, wanted %d", len(store.winCalls), 1)
-
-			if store.winCalls[0] != player {
-				t.Errorf("did not store the correct winner, got %q, want %q", store.winCalls[0], player)
-			}
-		}
+		AssertPlayerWin(t, &store, "Pepper")
 	})
 }
 
